d12d3dce4b9cbea6fd89774af20aa411059c6bcd
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 21:13:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 
 
 def reverse_each_word (sentence)
-arr_words = sentence.split #[hello, this, is, a, split, sentence]
+  arr_words = []
+  arr_words = sentence.split #[hello, this, is, a, split, sentence]
   arr_words.each do |word|
       word.reverse
   end
