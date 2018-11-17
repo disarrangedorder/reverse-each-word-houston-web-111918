e7d0f4d03f18c41ac820bed750e7bdeb34f4b234
Claire Nguyen <ms.clairenguyen@gmail.com>
2018-11-17 21:30:39 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,26 @@
+
+=begin
+def reverse_each_word (sentence)
+
+
+  arr_words = sentence.split #[hello, this, is, a, split, sentence]
+
+  arr_words.each do |word|
+    word.reverse!
+  end
+
+  arr_words.join(" ") #creates a string
+end
+=end
+
+
+def reverse_each_word (sentence)
+
+  arr_words = sentence.split #[hello, this, is, a, split, sentence]
+
+  arr_words.collect do |word|
+    word.reverse!
+  end
+
+  arr_words.join(" ") #creates a string
+end
